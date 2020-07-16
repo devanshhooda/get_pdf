292a9c54845e5ec69b02dd9ae481750db9d2e229
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2020-07-16 19:41:13 +0530

    revert

--- lib/services/pdfServices.dart
+++ lib/services/pdfServices.dart
@@ -3,7 +3,6 @@ import 'package:get_pdf/services/fileHandling.dart';
 import 'package:get_pdf/utils/constants.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:printing/printing.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PdfServices {
@@ -53,31 +52,6 @@ class PdfServices {
     thmb.writeAsBytesSync(image.readAsBytesSync()); // TODO: resize image
     print('Saved File: ${file.path}');
     return file.path;
-  }
-
-  Future<void> createThumbnails(File pdfFile) async {
-    try {
-      FileHandling handler = FileHandling();
-      await handler.initSystem();
-      File file = handler.getThumbFile(pdfFile.path);
-      var pdf = pdfFile.readAsBytesSync();
-      print(file);
-      // var page =
-      //     await Printing.raster(pdfFile.readAsBytesSync(), pages: [0], dpi: 30)
-      //         .first;
-      List<int> hhh = [0, 1];
-      var pages = Printing.raster(pdf, pages: hhh, dpi: 72).first;
-      // await for (var page in pages) {
-      //   print('hello');
-      // }
-      //print(page.pixels);
-      //var image = await page.toImage();
-      //var data = await image.toByteData();
-      //await file.writeAsBytes(page.pixels);
-    } catch (err) {
-      print('HERE');
-      print(err);
-    }
   }
 
   _checkFitProperty() {
